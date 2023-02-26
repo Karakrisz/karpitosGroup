0e72ca694e186a549ef4f97c550ddb9d45e61cb7
Boaz <webevolution13@gmail.com>
2023-02-26 12:29:03 +0100

    Front end
    External code

---
+++ templates/header.php
@@ -0,0 +1,71 @@
+<header class="main-header clearfix">
+    <nav class="main-menu clearfix">
+        <div class="main-menu-wrapper clearfix">
+            <div class="main-menu-wrapper-inner clearfix">
+                <div class="main-menu-wrapper__left clearfix">
+                    <div class="main-menu-wrapper__logo">
+                        <a href="index.html"><img src="assets/images/resources/logo-1.jpg" alt=""></a>
+                    </div>
+                    <div class="main-menu-wrapper__main-menu">
+                        <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                        <ul class="main-menu__list">
+                            <li class="dropdown current">
+                                <a href="index.html">Home</a>
+                                <ul>
+                                    <li>
+                                        <a href="index.html">Home One</a>
+                                    </li>
+                                    <li><a href="index2.html">Home Two</a></li>
+                                    <li class="dropdown">
+                                        <a href="#">Header Styles</a>
+                                        <ul>
+                                            <li><a href="index.html">Header One</a></li>
+                                            <li><a href="index.html">Header Two</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="about.html">About</a></li>
+                            <li class="dropdown">
+                                <a href="#">Services</a>
+                                <ul>
+                                    <li><a href="services.html">Services</a></li>
+                                    <li><a href="service-details.html">Service Details</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#">Projects</a>
+                                <ul>
+                                    <li><a href="project.html">Projects</a></li>
+                                    <li><a href="project-details.html">Project Details</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#">Blog</a>
+                                <ul>
+                                    <li><a href="news.html">News</a></li>
+                                    <li><a href="news-details.html">News Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="main-menu-wrapper__right">
+                    <div class="main-menu-wrapper__call">
+                        <div class="main-menu-wrapper__call-icon">
+                            <span class="icon-phone-call"></span>
+                        </div>
+                        <div class="main-menu-wrapper__call-number">
+                            <p>Call anytime</p>
+                            <a href="tel:926668880000">92 666 888 0000</a>
+                        </div>
+                    </div>
+                    <div class="main-menu-wrapper__search-box">
+                        <a href="#" class="main-menu-wrapper__search search-toggler icon-magnifying-glass"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
